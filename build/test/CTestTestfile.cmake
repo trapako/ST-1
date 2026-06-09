9436070f181a024388a1936500af6000f6e004e9
trapako <trapako@users.noreply.github.com>
2026-06-09 23:31:08 +0300

    Add .gitignore

--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/USER/Desktop/ST-1-1/test
-# Build directory: C:/Users/USER/Desktop/ST-1-1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
